4a9b6740f4f91674e8f640331adb2ff43cbb5073
Lucas Santana <lukkas_da_silva_santana@hotmail.com>
2020-04-17 19:58:24 -0400

    Update exemplo11.php

--- exemplo11.php
+++ exemplo11.php
@@ -9,4 +9,19 @@ foreach($cores as $valor){
     echo $valor;
 }
 
+echo "<h1>Percorrendo um array forma 2</h1>";
+$cores = ["azul","vermelho","amarelo","verde"];
+foreach($cores as $chave => $valor){
+    echo "$chave:$valor <br>";
+}
+
+echo "<h1> Percorrendo um dicionario</h1>";
+$arr = [ 
+    "nome"=>"Roberto",
+    "sobrenome"=>"Junior",
+    "idade"=>23
+];
+foreach($arr as $chave => $valor){
+    echo $chave."=>".$valor."<br/>";
+}
 ?>
