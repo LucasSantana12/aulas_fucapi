b41d65b71ec6fb0332ff2316c14e051a59af5c63
Lucas Santana <lukkas_da_silva_santana@hotmail.com>
2020-04-17 20:17:47 -0400

    Update exemplo11.php

--- exemplo11.php
+++ exemplo11.php
@@ -25,3 +25,49 @@ foreach($arr as $chave => $valor){
     echo $chave."=>".$valor."<br/>";
 }
 ?>
+
+<table style="width:100%" border="1">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
+
+
+<br><br>
+
+<table border='1' style="width:100%">
+
+<?php
+$tabela =[
+    ["nome"=>"Roberto","sobrenome"=>"Junior","idade"=>23],
+    ["nome"=>"Maria","sobrenome"=>"Eduarda","idade"=>25],
+    ["nome"=>"Matheus","sobrenome"=>"Silva","idade"=>18]
+];
+
+echo "<tr>";
+foreach($tabela[0] as $chaves => $valor){
+    echo "<th>$chave</th>";
+}
+echo "</tr>";
+
+foreach($tabela as $linhas){
+    echo "<tr>";
+     foreach($linhas as $valor){
+         echo "<td>$valor</td>";
+     }
+     echo "</tr>";
+}
+?>
+</table>
